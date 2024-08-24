b0bbfadaafa95f62d11599bc8a3e4dd8a52fb3bb
arjunmalhotra1 <am2cj@virginia.edu>
2024-08-24 11:14:08 -0400

    using chalk library

---
+++ Week-1-HTML-CSS-BasicJS/1.2/index.js
@@ -0,0 +1,5 @@
+var a = 1
+a = "hello world"
+a = true
+
+console.log(a) 
---
+++ Week-3/3.1-NodeJS/index.js
@@ -0,0 +1,15 @@
+// let firstName = "Arjun Malhotra"
+
+// console.log(firstName)
+
+// function sum(a,b) {
+//     return a+b
+// }
+
+// console.log(sum(3,2))
+
+import chalk from 'chalk';
+
+console.log(chalk.blue('Hello, world!'));
+console.log(chalk.red.bold('This is an error message.'));
+console.log(chalk.green.underline('This is a success message.'));
