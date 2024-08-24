8bf78416cd754efd580ddd32e4257b3734f0113d
arjunmalhotra1 <am2cj@virginia.edu>
2024-08-24 11:27:55 -0400

    internal path library

---
+++ Week-3/3.1-NodeJS/index2.js
@@ -0,0 +1,13 @@
+const path = require("path")
+
+console.log(__dirname)
+console.log(__dirname+"../../../index2.js")
+console.log(path.join(__dirname,"../../../index2.js"))
+
+
+/*
+node index2.js 
+/Users/arjunmalhotra/Documents/repos/self-learning/web-2-kirat/Week-3/3.1-NodeJS
+/Users/arjunmalhotra/Documents/repos/self-learning/web-2-kirat/Week-3/3.1-NodeJS../../../index2.js
+/Users/arjunmalhotra/Documents/repos/self-learning/index2.js
+ */
