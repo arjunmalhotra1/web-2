f8f2da01dba818ad6a1a86ac00e7ee6270c37fec
arjunmalhotra1 <am2cj@virginia.edu>
2024-08-29 09:11:46 -0400

    checkpoint

--- Week-2/2.1-Asynch/index.js
+++ Week-2/2.1-Asynch/index.js
@@ -1,26 +1,106 @@
-function sum(a, b) {
-    return a+b
-}
+// function sum(a, b) {
+//     return a+b
+// }
 
-let ans = sum(20,30)
+// let ans = sum(20,30)
 
-console.log(ans)
+// console.log(ans)
 
-function sum1(n) {
-    let sum = 0
-    for (let i=1;i<=n;i++) {
-        sum += i
-    }
+// function sum1(n) {
+//     let sum = 0
+//     for (let i=1;i<=n;i++) {
+//         sum += i
+//     }
 
-    return sum
-}
+//     return sum
+// }
 
-console.log(sum1(123))
+// console.log(sum1(123))
 
-function sum2(n) {
+// function sum2(n) {
     
-    return (n *(n+1))/2
+//     return (n *(n+1))/2
+// }
+
+// console.log(sum2(123))
+
+
+
+// function sum(n) {
+// 	let ans = 0;
+// 	for (let i = 1; i <= n; i++) {
+// 		ans = ans + i
+// 	}
+// 	return ans;
+// }
+
+// const ans1 = sum(100);
+// console.log(ans1);
+// const ans2 = sum(1000);
+// console.log(ans2);
+// const ans3 = sum(10000);
+// console.log(ans3);
+
+
+// Reading file a.txt
+// const fs = require("fs")
+
+// const contents = fs.readFileSync("a.txt","utf-8")
+// console.log(contents)
+
+// const contentsb = fs.readFileSync("b.txt", "utf-8")
+// console.log(contentsb)
+
+// const fs = require("fs");
+
+// const contents = fs.readFile("a.txt","utf-8");
+// console.log(contents)
+
+// const contentsb = fs.readFileSync("b.txt","utf-8");
+// console.log(contentsb)
+
+
+// function sum(a, b) {
+//     return a+b
+// }
+
+
+// function doOperation(a,b,op) {
+//     return op(a,b)
+// }
+
+// console.log(doOperation(1,2,sum))
+
+
+const fs = require("fs");
+
+// afterFileRead is the callback function.
+function afterFileRead(err, data) {
+    console.log(data)
 }
 
-console.log(sum2(123))
+/*
+better way to write the afterFileRead function.
+function afterFileRead(err, data) {
+    if (err) {
+        console.log("File not found!")
+    } else {
+        console.log(data)
+    }
+}
+*/
 
+fs.readFile("a.txt","utf-8",afterFileRead);
+fs.readFile("b.txt","utf-8",afterFileRead);
+console.log("Done");
+
+
+/* 
+// This is how the designers of readFile might have written the function as
+function readFile(filePath, encoding, op) {
+    // read file and then call the callback with an error if there is an error & then with the 
+    // actual contents of the file.
+
+    op("Error","hi there")
+}
+*/
