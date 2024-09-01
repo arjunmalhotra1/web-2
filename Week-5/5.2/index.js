70437099bad8e872c648b6d8d5e0c59257c3bd19
arjunmalhotra1 <am2cj@virginia.edu>
2024-09-01 11:29:29 -0400

    Simple post request in express.

--- Week-5/5.2/index.js
+++ Week-5/5.2/index.js
@@ -2,53 +2,22 @@ const express = require("express");
 
 const app = express();
 
-
-function loggerMiddleware(req,res, next) {
-    console.log("Method is: ", req.method);
-    console.log("URL is: ",req.url);
-    console.log("Hostname is: ",req.hostname);
-    console.log(new Date());
-
-    next();
-}
+app.use(express.json());
 
 
-
-app.get("/sum", function(req, res) {
-    const a = req.query.a;
-    const b = req.query.b;
+// In express if you want to send json data we need to first parse the json data.
+app.post("/sum", function(req, res) {
+    console.log(req.body)
+    const a = req.body.a;
+    const b = req.body.b;
 
     res.json({
         ans: a + b
     })
 });
 
-// All the route handlers after the app.use get the middleware.
-app.use(loggerMiddleware)
 
-app.get("/multiply", function(req, res) {
-    const a = req.query.a;
-    const b = req.query.b;
-    res.json({
-        ans: a * b
-    })
-});
 
-app.get("/divide", function(req, res) {
-    const a = req.query.a;
-    const b = req.query.b;
-    res.json({
-        ans: a / b
-    })
 
-});
-
-app.get("/subtract", function(req, res) {
-    const a = req.query.a;
-    const b = req.query.b;
-    res.json({
-        ans: a - b
-    })
-});
 
 app.listen(3009);
