9429b0ef3076c7b5a1ef0778f776f21e9b95d52f
arjunmalhotra1 <am2cj@virginia.edu>
2024-09-07 11:15:36 -0400

    checkpoint - JWT

--- Week-6/6.1-JWTs/index.js
+++ Week-6/6.1-JWTs/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const app = express();
+const JWT_SECRET = "USER_APP";
 app.use(express.json());
 
 
@@ -26,7 +28,11 @@ app.post("/signin",(req, res)=>{
     const user = users.find(user => user.username === username && user.password === password);
      
     if (user) {
-        const token = generateToken();
+        // const token = generateToken();
+        const token = jwt.sign({
+            username: user.username
+        }, JWT_SECRET);
+
         user.token = token;
         res.send({
             token
@@ -42,7 +48,11 @@ app.post("/signin",(req, res)=>{
 
 app.get("/me",(req,res)=> {
     const token = req.headers.authorization;
-    const user = users.find(user => user.token === token);
+    //const user = users.find(user => user.token === token);
+    const userDetails = jwt.verify(token, JWT_SECRET);
+    const username =  userDetails.username;
+
+    const user = users.find(user => user.username === username);
     if (user) {
         res.send({
             username: user.username,
@@ -55,17 +65,27 @@ app.get("/me",(req,res)=> {
     }
 })
 
-function generateToken() {
-    let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+// function generateToken() {
+//     let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-    let token = ""
-    for (let i = 0; i < 32; i++) {
-        // use a simple function here
-        token += options[Math.floor(Math.random() * options.length)];
-    }
-    return token;
+//     let token = ""
+//     for (let i = 0; i < 32; i++) {
+//         // use a simple function here
+//         token += options[Math.floor(Math.random() * options.length)];
+//     }
+//     return token;
 
-}
+// }
 
 
 app.listen(3087)
+
+
+// for( leti = 0;i<users.length;i++) {
+//     if (users[i].username == username && )
+// }
+
+
+// users.find(function(u){
+    
+// })
