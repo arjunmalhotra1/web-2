8d261f1b33fffb451b164bd0dfb7711efe89a776
arjunmalhotra1 <am2cj@virginia.edu>
2024-09-20 08:19:15 -0400

    checkpoint

--- Week-3/3.2-DOM-Simple/script.js
+++ Week-3/3.2-DOM-Simple/script.js
@@ -1,7 +1,10 @@
 let counter = 0
+
 function callback() {
-    console.log(counter);
-    counter = counter + 1
+    const el = document.querySelectorAll("h4")[1]
+    el.innerHTML = counter;
+    //console.log(counter);
+    counter = counter + 1;
 }
 
-setInterval(callback, 1000)
+setInterval(callback, 1000);
