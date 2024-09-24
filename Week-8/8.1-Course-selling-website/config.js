02c8568173a286c12b0b1f8cda0a92cbdce0dbce
arjunmalhotra1 <am2cj@virginia.edu>
2024-09-23 23:19:22 -0400

    ad config file

--- Week-8/8.1-Course-selling-website/config.js
+++ Week-8/8.1-Course-selling-website/config.js
@@ -1,5 +1,7 @@
-const JWT_USER_PASSWORD = "user_secret";
-const JWT_ADMIN_PASSWORD = "admin_secret";
+require('dotenv').config()
+
+const JWT_USER_PASSWORD = process.env.JWT_USER_PASSWORD
+const JWT_ADMIN_PASSWORD = process.env.JWT_USER_PASSWORD;
 
 module.exports = {
     JWT_USER_PASSWORD,
