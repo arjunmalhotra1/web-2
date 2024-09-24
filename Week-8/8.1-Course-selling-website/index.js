67232ba605abb214512e32f1ffead93b9183c1b1
arjunmalhotra1 <am2cj@virginia.edu>
2024-09-23 23:19:40 -0400

    fix endpoints

--- Week-8/8.1-Course-selling-website/index.js
+++ Week-8/8.1-Course-selling-website/index.js
@@ -3,16 +3,17 @@ const mongoose = require("mongoose")
 const { userRouter } = require('./routes/user');
 const { courseRouter } = require('./routes/course');
 const { adminRouter } = require('./routes/admin');
+require('dotenv').config()
 
 const app = express()
 app.use(express.json());
 
 app.use("/api/v1/user", userRouter)
-app.use("api/v1/course", courseRouter)
-app.use("api/v1/admin", adminRouter)
+app.use("/api/v1/course", courseRouter)
+app.use("/api/v1/admin", adminRouter)
 
 async function main() {
-    await mongoose.connect()
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("listening on port 3037")
     app.listen(3037)
 }
