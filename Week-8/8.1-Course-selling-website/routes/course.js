592fc8b4b31ff8004e69193be95075c440a35d8f
arjunmalhotra1 <am2cj@virginia.edu>
2024-09-25 15:16:19 -0400

    implement course endpoints

--- Week-8/8.1-Course-selling-website/routes/course.js
+++ Week-8/8.1-Course-selling-website/routes/course.js
@@ -1,30 +1,49 @@
 const { Router } = require("express")
 const courseRouter = Router()
-const { purchaseModel } = require("../db");
+const { purchaseModel, courseModel } = require("../db");
 const { userMiddleware } = require('../middleware/user')
+const {z} = require("zod")
 
 // user purchasing a course
 courseRouter.post('/purchase',userMiddleware, async function(req, res) {
-    
-    const userId = req.userId;
+
+    const requiredBody = z.object({
+        courseId: z.string().min(3).max(30)
+    })
+
+    const parsedDataWithSuccess = requiredBody.safeParse(req.body)
+    if (!parsedDataWithSuccess.success) {
+        res.status(404).json({
+            message: parsedDataWithSuccess.error      
+        })
+    }
+    const userId = req.userid;
     const courseId = req.body.courseId;
 
-    await purchaseModel.create({
+   const purchased =  await purchaseModel.create({
         userId,
         courseId
     })
 
-
-    // you would expect the user to pay you money
-    res.json({
-        message: "you have successfully bought the course"
-    })
+    // TODO: Handle payment & Handle error for if the course with provided courseId is not found.
+    if (purchased) {
+        res.json({
+            message: "you have successfully bought the course"
+        })
+    } else {
+        res.status(500).json({
+            message: "the request couldn't be completed"
+        })
+    }
+    
 
 })
 
-courseRouter.get('/preview',function(req, res) {
+courseRouter.get('/preview',async function(req, res) {
+    const courses = await courseModel.find({});
+
     res.json({
-        message: "course preview"
+        courses
     })
 })
 
