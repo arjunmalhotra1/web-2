be95e955a0ed9c0e448a3ca049980df0ab91ab39
arjunmalhotra1 <am2cj@virginia.edu>
2024-10-12 09:41:09 -0400

    posts is not a state component

--- week-9-offline/src/App.jsx
+++ week-9-offline/src/App.jsx
@@ -1,6 +1,7 @@
 // import { useState  } from "react";
 import { PostComponent } from "./Post";
 function App() {
+  
 
   const posts = [{
     name: "chelsea",
@@ -11,17 +12,25 @@ function App() {
   }]
 
   const postComponents = posts.map(post => <PostComponent 
-    name= {post.name}
-    subtitle= {post.subtitle}
-    time= {post.time}
-    image= {post.image}
-    description= {post.description}
+    name={post.name}
+    subtitle={post.subtitle}
+    time={post.time}
+    image={post.image}
+    description={post.description}
     />
   )
 
   function addPost() {
+    posts.push({
+    name: "chelsea",
+    subtitle: "23,888 followers",
+    time: "2m ago",
+    image: "https://www.shutterstock.com/image-vector/chelsea-football-club-fc-icon-600nw-2276198713.jpg",
+    description: "Next match is vs Liverpool"
+    })
 
   }
+
   return (
     <div style={{background: "#dfe6e9",height: "100vh" }}>
       <button onClick={addPost}>Add Post </button>
