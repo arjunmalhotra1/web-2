46c5875ebd0011288efe63d0a9d1156b18c47937
arjunmalhotra1 <am2cj@virginia.edu>
2024-10-12 12:00:22 -0400

    wrong way to stop the clock.

--- Week-10/10.1/week-10-routing/src/App.jsx
+++ Week-10/10.1/week-10-routing/src/App.jsx
@@ -1,10 +1,27 @@
+import {useState} from 'react'
 import './App.css'
 
 function App() {
-  const   
+  const [currentCount, setCurrentCount] = useState(0)
+  let timer = 0
+
+  function startClock() {
+    timer = setInterval(function() {
+      setCurrentCount(c => c+1)
+    }, 1000)
+  }
+
+  function stopClock() {
+    console.log(timer)
+    clearInterval(timer)
+  }
 
   return <div>
-
+    {currentCount}
+    <br/>
+    <button onClick={startClock}>Start</button>
+    <br/>
+    <button onClick={stopClock}>Stop</button>
     </div>
 }
 
