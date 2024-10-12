c6a4f921605d7fa17231636ce2e04279725b8e0b
arjunmalhotra1 <am2cj@virginia.edu>
2024-10-12 11:01:49 -0400

    naive way to reroute

--- Week-10/10.1/week-10-routing/src/App.jsx
+++ Week-10/10.1/week-10-routing/src/App.jsx
@@ -5,12 +5,33 @@ import './App.css'
 function App() {
 
   return <div>
+    <a href="/">Allen</a> | <a href="/neet/online-coaching-class-11">Class 11</a> | <a href="/neet/online-coaching-class-12">Class 12</a>
     <BrowserRouter>
       <Routes>
-        
+        <Route path="/neet/online-coaching-class-11" element={<Class11Program />} />
+        <Route path="/neet/online-coaching-class-12" element={<Class12Program />} />
+        <Route path="/" element={<Landing />} />
       </Routes>
     </BrowserRouter>
     </div>
+}
+
+function Landing() {
+  return <div>
+    Welcome to Allen
+  </div>
+}
+
+function Class11Program() {
+  return <div>
+    NEET program for Class 11th
+  </div>
+}
+
+function Class12Program() {
+  return <div>
+    NEET program for Class 12th
+  </div>
 }
 
 export default App
