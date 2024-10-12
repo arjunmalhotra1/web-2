c65eee22e016e425777c7c70f31a5ddff703b2ee
arjunmalhotra1 <am2cj@virginia.edu>
2024-10-12 11:45:49 -0400

    useRef - To focus on an input box

--- Week-10/10.1/week-10-routing/src/App.jsx
+++ Week-10/10.1/week-10-routing/src/App.jsx
@@ -1,16 +1,19 @@
-import { useState } from 'react'
-import { BrowserRouter, Routes, Route, Link, useNavigate, Outlet } from "react-router-dom"
+import { useState, useRef } from 'react'
+import { BrowserRouter, Routes, Route, Link, useNavigate, Outlet  } from "react-router-dom"
 import './App.css'
 
 function App() {
+  const inputRef = useRef();
+  
 
   function focusOnInput() {
-    document.getElementById("name").focus()
+    // document.getElementById("name").focus()
+    inputRef.current.focus();
   }
 
   return <div>
     Sign up
-    <input id="name" type={"text"}></input>
+    <input ref={inputRef} id="name" type={"text"}></input>
     <input type={"text"}></input>
     <button onClick={focusOnInput}> submit</button>
     </div>
