22250c5e9fb437f0daa100b6109ba2855cab3e96
arjunmalhotra1 <am2cj@virginia.edu>
2024-10-12 08:19:21 -0400

    State variable vs not a state variable

--- week-9-offline/src/App.jsx
+++ week-9-offline/src/App.jsx
@@ -1,8 +1,86 @@
-
+import { useState  } from "react";
 function App() {
   return (
     <div style={{background: "#dfe6e9",height: "100vh" }}>
-      <div style={{display:"flex", justifyContent: "center"}}>
+      <ToggleMessage />
+    </div>
+  )
+}
+
+const ToggleMessage = () => {
+    //const [isVisible, setIsVisible] = useState(false); //defining a new state variable
+    // When the value of the state variable changes the component aht uses the state variable, re-renders
+
+    // const isVisible = true; // This is  not a state variable
+
+    return (
+        <div>
+            <button onClick={() => setIsVisible(!isVisible)}>
+                Toggle Message
+            </button>
+            {isVisible && <p>This message is conditionally rendered!</p>}
+        </div>
+    );
+};
+
+// Not a state variable. This doesn't toggle. The component isn't re-rendering because we don't use a 
+// state variable.
+// const ToggleMessage = () => {
+//   let isVisible = true; // This is  not a state variable
+
+//   function toggle() {
+//     isVisible = !isVisible
+//   }
+
+//   return (
+//       <div>
+//           <button onClick={toggle}>
+//               Toggle Message
+//           </button>
+//           {isVisible && <p>This message is conditionally rendered!</p>}
+//       </div>
+//   );
+// };
+
+
+
+
+
+
+// const style = {width: 200, backgroundColor: "white", borderRadius: 10, borderColor: "gray",borderWidth:1, padding:20 }
+// 
+// 
+// function PostComponent({name, subtitle, time, image, description}) {
+    // return <div style={style}>
+    {/* <div style={{display:"flex "}}> */}
+      {/* <img src={image} */}
+      // style={{
+        // width:40,
+        // height: 40,
+        // borderRadius: 200
+      // }}/>
+{/*  */}
+      {/* <div style={{fontSize: 10, marginLeft:10}}> */}
+        {/* <b> */}
+          {/* {name} */}
+        {/* </b> */}
+      {/* <div>{subtitle}</div> */}
+     {/* {time!== undefined ? <div style={{display: 'flex'}}> */}
+        {/* <div>{time}</div> */}
+        {/* <img src={"https://i.etsystatic.com/19543171/r/il/cd0d78/4477428278/il_570xN.4477428278_i2tn.jpg"} */}
+        // style={{width: 12  , height: 12}} />
+      {/* </div>:null} */}
+      {/* </div> */}
+    {/* </div> */}
+{/*  */}
+    {/* <div style={{fontSize: 12}}> */}
+    {/* Win lose or tie.... The next match is vs Liverpool. */}
+    {/* </div> */}
+{/* </div> */}
+// }
+
+
+{/* <div style={{display:"flex", justifyContent: "center"}}>
         <div>
           <div>
             <PostComponent 
@@ -21,43 +99,8 @@ function App() {
             />
             <br />
           </div>
-      </div>
-      </div>
-    </div>
-  )
-}
-
-const style = {width: 200, backgroundColor: "white", borderRadius: 10, borderColor: "gray",borderWidth:1, padding:20 }
-
-
-function PostComponent({name, subtitle, time, image, description}) {
-    return <div style={style}>
-    <div style={{display:"flex "}}>
-      <img src={image}
-      style={{
-        width:40,
-        height: 40,
-        borderRadius: 200
-      }}/>
-
-      <div style={{fontSize: 10, marginLeft:10}}>
-        <b>
-          {name}
-        </b>
-      <div>{subtitle}</div>
-     {time!== undefined ? <div style={{display: 'flex'}}>
-        <div>{time}</div>
-        <img src={"https://i.etsystatic.com/19543171/r/il/cd0d78/4477428278/il_570xN.4477428278_i2tn.jpg"}
-        style={{width: 12  , height: 12}} />
-      </div>:null}
-      </div>
-    </div>
-
-    <div style={{fontSize: 12}}>
-    Win lose or tie.... The next match is vs Liverpool.
-    </div>
-</div>
-}
+        </div>
+      </div> */}
 
 
 // Structuring you app into components
