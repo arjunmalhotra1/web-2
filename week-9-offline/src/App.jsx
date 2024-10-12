d478d1a98c6863dc0a0baf3fecf49ccb9cfbfae7
arjunmalhotra1 <am2cj@virginia.edu>
2024-10-12 07:50:45 -0400

    Promoted component vs Followers component.

--- week-9-offline/src/App.jsx
+++ week-9-offline/src/App.jsx
@@ -5,17 +5,22 @@ function App() {
       <div style={{display:"flex", justifyContent: "center"}}>
         <div>
           <div>
-            <PostComponent />
+            <PostComponent 
+             image = "https://www.shutterstock.com/image-vector/chelsea-football-club-fc-icon-600nw-2276198713.jpg"
+             subtitle = "23,888 followers"
+             time = "12m"
+              name = {"chelsea"}
+            />
             <br />
           </div>
           <div>
-            <PostComponent />
+            <PostComponent 
+             image = "https://www.shutterstock.com/image-vector/chelsea-football-club-fc-icon-600nw-2276198713.jpg"
+             subtitle = "Promoted"
+              name = {"chelsea"}
+            />
             <br />
           </div>
-          <div>
-            <PostComponent />
-            <br />
-          </div> 
       </div>
       </div>
     </div>
@@ -24,10 +29,11 @@ function App() {
 
 const style = {width: 200, backgroundColor: "white", borderRadius: 10, borderColor: "gray",borderWidth:1, padding:20 }
 
-function PostComponent() {
+
+function PostComponent({name, subtitle, time, image, description}) {
     return <div style={style}>
     <div style={{display:"flex "}}>
-      <img src={"https://www.shutterstock.com/image-vector/chelsea-football-club-fc-icon-600nw-2276198713.jpg"}
+      <img src={image}
       style={{
         width:40,
         height: 40,
@@ -36,10 +42,14 @@ function PostComponent() {
 
       <div style={{fontSize: 10, marginLeft:10}}>
         <b>
-          chelsea
+          {name}
         </b>
-      <div>23,888 followers</div>
-      <div>12m</div>
+      <div>{subtitle}</div>
+      <div style={{display: 'flex'}}>
+        <div>{time}</div>
+        <img src={"https://i.etsystatic.com/19543171/r/il/cd0d78/4477428278/il_570xN.4477428278_i2tn.jpg"}
+        style={{width: 12  , height: 12}} />
+      </div>
       </div>
     </div>
 
