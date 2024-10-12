8d41178363aebe634b44925a0c62e89ddc159e47
arjunmalhotra1 <am2cj@virginia.edu>
2024-10-12 10:54:16 -0400

    Using Browser Routes

--- Week-10/10.1/week-10-routing/src/App.jsx
+++ Week-10/10.1/week-10-routing/src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
-
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import './App.css'
 
 function App() {
 
-
   return <div>
-     hi There 
+    <BrowserRouter>
+      <Routes>
+        
+      </Routes>
+    </BrowserRouter>
     </div>
 }
 
