59670e4bd36ae1d071b1d5a74a78a883b9c2b9ee
arjunmalhotra1 <am2cj@virginia.edu>
2024-10-12 11:23:34 -0400

    checkpoint

--- Week-10/10.1/week-10-routing/src/App.jsx
+++ Week-10/10.1/week-10-routing/src/App.jsx
@@ -1,23 +1,32 @@
 import { useState } from 'react'
-import { BrowserRouter, Routes, Route, Link, useNavigate } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Link, useNavigate, Outlet } from "react-router-dom"
 import './App.css'
 
 function App() {
 
   return <div>
     <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />} />
+          <Route path="/neet/online-coaching-class-11" element={<Class11Program />} />
+          <Route path="/neet/online-coaching-class-12" element={<Class12Program />} />
+          <Route path="/" element={<Landing />} />
+          <Route path="/*" element={<ErrorPage />} />
+      </Routes>
+    </BrowserRouter>
+    </div>
+}
+
+function Layout() {
+  return <div>
     <Link to="/">Allen</Link> | 
     <Link to="/neet/online-coaching-class-11">Class 11</Link> | 
     <Link to="/neet/online-coaching-class-12">Class 12</Link>
-      <Routes>
-        <Route path="/neet/online-coaching-class-11" element={<Class11Program />} />
-        <Route path="/neet/online-coaching-class-12" element={<Class12Program />} />
-        <Route path="/" element={<Landing />} />
-        <Route path="/*" element={<ErrorPage />} />
-      </Routes>
-      Footer | Contact Us
-    </BrowserRouter>
-    </div>
+    header
+    <Outlet />
+    <div style={{height: "90vh"}}> </div>
+    footer
+  </div>
 }
 
 function ErrorPage() {
