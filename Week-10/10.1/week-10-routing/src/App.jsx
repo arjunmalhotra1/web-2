e336b431b7628a95d891736b8ee933a780cc3ce7
arjunmalhotra1 <am2cj@virginia.edu>
2024-10-12 11:51:57 -0400

    Build the stopwatch

--- Week-10/10.1/week-10-routing/src/App.jsx
+++ Week-10/10.1/week-10-routing/src/App.jsx
@@ -1,21 +1,10 @@
-import { useState, useRef } from 'react'
-import { BrowserRouter, Routes, Route, Link, useNavigate, Outlet  } from "react-router-dom"
 import './App.css'
 
 function App() {
-  const inputRef = useRef();
-  
-
-  function focusOnInput() {
-    // document.getElementById("name").focus()
-    inputRef.current.focus();
-  }
+  const   
 
   return <div>
-    Sign up
-    <input ref={inputRef} id="name" type={"text"}></input>
-    <input type={"text"}></input>
-    <button onClick={focusOnInput}> submit</button>
+
     </div>
 }
 
