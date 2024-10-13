af09ac29e3822f0c727b9aa2a801696d6fe768d3
arjunmalhotra1 <am2cj@virginia.edu>
2024-10-12 20:57:11 -0400

    upload side effect snapshot from slides.

--- week-9-offline/src/App.jsx
+++ week-9-offline/src/App.jsx
@@ -2,14 +2,8 @@ import { useState  } from "react";
 import { PostComponent } from "./Post";
 
 
-
-// These codes don't have side effects. without the setInterval
 function App() {
 
-  // In the last section we were trying to increment every one second.
-  // This is a side effect, it has nothing to do with the component's rendering
-  // we are starting a clock independently. This is what is called a side effect.
-  // Another side effect could be fetching data from an api.
   setInterval(increments, 1000)
   return <div>
     hi there
