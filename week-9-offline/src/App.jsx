a3f26fae50a74f75447cc227d7c4b420d1f9f544
arjunmalhotra1 <am2cj@virginia.edu>
2024-10-12 21:23:11 -0400

    Problem without useEffect. So many rendering.

--- week-9-offline/src/App.jsx
+++ week-9-offline/src/App.jsx
@@ -2,26 +2,17 @@ import { useState  } from "react";
 import { PostComponent } from "./Post";
 
 
-// Every time the app component rerenders the fetch and the setInterval functions are run again.
-// It can have unnecessary or duplicated effects.
-// Inconsistent behavior.
-// Performance issues. Could also cause excessive rerendering.
 function App() {
+  const [count, setCount] = useState(1)
 
-  setInterval()
-  fetch()
+  function increaseCount() {
+    setCount(count+1)
+  }
 
-  setInterval(increments, 1000)
+  setInterval(increaseCount, 1)
+  
   return <div>
-    hi there
-    <Greeting name={"harkirat"} />
-    <Greeting name={"raman"} />
-  </div>  
-}
-
-function Greeting(props) {
-  return <div>
-    Hi There {props.name}
+    {count}
   </div>
 }
 
