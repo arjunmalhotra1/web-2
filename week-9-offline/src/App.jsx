cb28477159f4c40a4d7a108f4895aa4e52079727
arjunmalhotra1 <am2cj@virginia.edu>
2024-10-12 20:51:26 -0400

    9.2 simple react rendering

--- week-9-offline/src/App.jsx
+++ week-9-offline/src/App.jsx
@@ -5,27 +5,17 @@ import { PostComponent } from "./Post";
 
 function App() {
 
-  function increaseCount() {
-    setCount(count + 1)
-  }
-
-
-  setInterval(increaseCount, 1000)
-
-  const [count, setCount] = useState(1)
-
   return <div>
-  <div style={{display: "flex"}}>
-    <div style={{background: "red",borderRadius: 20, width:20, height: 25 , paddingLeft: 10, paddingTop: 5}}>
-      {count}
-    </div>
-  </div>
-  <img style={{cursor: "pointer"}} src={"https://cdn-icons-png.flaticon.com/512/472/472371.png"} width={40} />
+    hi there
+    <Greeting name={"harkirat"} />
+    <Greeting name={"raman"} />
+  </div>  
+}
 
-  
+function Greeting(props) {
+  return <div>
+    Hi There {props.name}
   </div>
-
-  
 }
 
 
