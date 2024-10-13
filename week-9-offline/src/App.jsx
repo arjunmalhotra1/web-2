0d37462d358d26f5c5fc046cd560a9cb612f5e62
arjunmalhotra1 <am2cj@virginia.edu>
2024-10-12 21:02:02 -0400

    Notes about problems with side effects.

--- week-9-offline/src/App.jsx
+++ week-9-offline/src/App.jsx
@@ -2,7 +2,14 @@ import { useState  } from "react";
 import { PostComponent } from "./Post";
 
 
+// Every time the app component rerenders the fetch and the setInterval functions are run again.
+// It can have unnecessary or duplicated effects.
+// Inconsistent behavior.
+// Performance issues. Could also cause excessive rerendering.
 function App() {
+
+  setInterval()
+  fetch()
 
   setInterval(increments, 1000)
   return <div>
