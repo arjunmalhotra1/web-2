cefeb47c70d7a7511bb6459809e6a6ccef5f7433
arjunmalhotra1 <am2cj@virginia.edu>
2024-10-13 09:07:41 -0400

    Using Fetch with useEffect

--- week-9-offline/src/App.jsx
+++ week-9-offline/src/App.jsx
@@ -3,19 +3,32 @@ import { useState, useEffect  } from "react";
 
 function App() {
 
-  const [currentTab, SetCurrentTab] = useState("feed")
+  const [currentTab, SetCurrentTab] = useState(1)
+  const [tabData, SetTabData] = useState({})
 
-  // When a user clicks on any of the button the screen on Linkedin changes for which we need a useEffect for the request to go out to the backend.
+  // When a user clicks on any of the button the screen on Linkedin changes for which we need a useEffect for the request to go out to the backend. We can lazily load the data from the backend, that is after the user 
+  // click the tab we send a fetch request.
   useEffect(function(){
     console.log("send request to backend to get data for tab " + currentTab)
+    
+    // We can send a request to the backend using Fetch inside useEffect.
+    fetch("https://jsonplaceholder.typicode.com/todos/"+currentTab).then(async res => {
+      const json = await res.json()
+      SetTabData(json)
+    })
+
   }, [currentTab])
 
   // Note onClick takes a function.
   return <div>
-    <button onClick={function(){SetCurrentTab("feed")}} style={{color: currentTab=="feed"? "red": "black"}}>Feed</button>
-    <button onClick={function(){SetCurrentTab("notifications")}} style={{color: currentTab=="notifications"?"red":"black"}}>Notifications</button>
-    <button onClick={function(){SetCurrentTab("messages")}} style={{color: currentTab=="messages"?"red":"black"}}>Messages</button>
-    <button onClick={function(){SetCurrentTab("jobs")}} style={{color: currentTab=="jobs"?"red":"black"}}>Jobs</button>
+    <button onClick={function(){SetCurrentTab(1)}} style={{color: currentTab==1? "red": "black"}}>Todo #1</button>
+    <button onClick={function(){SetCurrentTab(2)}} style={{color: currentTab==2?"red":"black"}}>Todo #2</button>
+    <button onClick={function(){SetCurrentTab(3)}} style={{color: currentTab==3?"red":"black"}}>Todo #3</button>
+    <button onClick={function(){SetCurrentTab(4)}} style={{color: currentTab==4?"red":"black"}}>Todo #4</button>
+
+  <br></br>
+    {/* Fetch requests fetches the data from the backend */}
+    {tabData.title}
   </div>
 }
 
