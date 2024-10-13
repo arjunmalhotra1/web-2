504cb647c7642bc78384de42288c0d14087c8974
arjunmalhotra1 <am2cj@virginia.edu>
2024-10-12 21:44:30 -0400

    simple useEffect.

--- week-9-offline/src/App.jsx
+++ week-9-offline/src/App.jsx
@@ -4,7 +4,10 @@ import { useState, useEffect  } from "react";
 function App() {
   const [count, setCount] = useState(1)
   
+  // Whatever code we write inside the function that is taken as an argument by useEffect, only runs
+  // when the components mounts/renders for the very first time.
  useEffect(function(){
+  console.log("above set interval")
   setInterval(increaseCount, 1000)
  },[])
   
@@ -13,8 +16,6 @@ function App() {
     setCount(count+1)
   }
 
-  
-  
   return <div>
     {count}
   </div>
