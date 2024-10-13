75c5d1beecfa1e5d4bfaaaae01582b76c18ed17c
arjunmalhotra1 <am2cj@virginia.edu>
2024-10-12 22:42:43 -0400

    Adding one more effect with dependency array

--- week-9-offline/src/App.jsx
+++ week-9-offline/src/App.jsx
@@ -6,12 +6,18 @@ function App() {
 
  useEffect(function(){
   setInterval(increaseCount, 1000)
- },[]) //This effect will run on mount because the array is empty.
+ },[]) //This effect will runs only on mount because the array is empty.
+
+
+ useEffect(function(){
+  console.log("the count has been updated to: " + count)
+ },[count]) // This effect runs every time count changes. This is what dependency array tracts.
   
  
  function increaseCount() {
     setCount(currentValue => currentValue+1)
   }
+
 
   return <div>
     {count}
