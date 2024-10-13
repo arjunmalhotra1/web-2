7a4ba1169e3fec64ce57f55fcbc712182ef192c9
arjunmalhotra1 <am2cj@virginia.edu>
2024-10-13 10:47:09 -0400

    Adding comments.

--- Week-10/10.2/10-2-context-api/src/App.jsx
+++ Week-10/10.2/10-2-context-api/src/App.jsx
@@ -24,7 +24,9 @@ function BulbState() {
     {bulbOn ? "Bulb on":"Bulb off"}
   </div>
 }
-
+// We need to update the state variable in the ToggleBulbState, we need to roll it down.
+// We defined it in BulbState we want to use it on ToggleBulbState
+// We need to roll the state up.
 function ToggleBulbState() {
   return <div>
     <button> Toggle the button </button>
