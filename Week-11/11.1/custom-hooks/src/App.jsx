fed503c6dc318cdced05d043a82a710b6710875e
arjunmalhotra1 <am2cj@virginia.edu>
2024-10-19 11:09:02 -0400

    Using hook to a separate file.

--- Week-11/11.1/custom-hooks/src/App.jsx
+++ Week-11/11.1/custom-hooks/src/App.jsx
@@ -1,26 +1,15 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+
 import './App.css'
+import { usePostTile } from './hooks/useFetch'
 
 function App() {
-  const [post, setPost] = useState({})
   
-  async function getPosts() {
-    const response = await fetch("https://jsonplaceholder.typicode.com/posts/1")
-    const json = await response.json();
-    setPost(json)
-  }
-
-
-// In react function to useEffect cannot be async hence we take that function out.
-  useEffect(() => {
-    getPosts();
-  })
+  const postTitle = usePostTile()
 
   return (
     <div>
-       {post.title}
+       Title after hook in hooks folder: {postTitle}
     </div>
   )
 }
