e4a39013f85a9180091eebe45d4184105d221aad
arjunmalhotra1 <am2cj@virginia.edu>
2024-10-19 10:57:38 -0400

    cleanup for useFetch hook

--- Week-11/11.1/custom-hooks/src/App.jsx
+++ Week-11/11.1/custom-hooks/src/App.jsx
@@ -3,23 +3,6 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-// custom Hook - A function which has repeated/reuse logic encapsulated
-// custom hook uses another hook and starts with "use"
-function useCounter() {
-  const [count, setCount] = useState(0)
-
-  function increaseCount() {
-    setCount(count+1)
-    // Another way to do it setCount(c => c+1)
-  }
-
-  return {
-    count: count,
-    increaseCount: increaseCount
-  }
-}
-
-// Component now becomes clean
 function App() {
 
   return (
@@ -32,12 +15,5 @@ function App() {
 }
 
 
-function Counter() {
-  const {count, increaseCount} = useCounter()
-  return <div>
-        <button onclick={increaseCount}>Increase {count} </button>
-        <Counter />
-    </div>
-}
 
 export default App
