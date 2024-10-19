60c789c92f455dec884718c226cb52c420f8aed4
arjunmalhotra1 <am2cj@virginia.edu>
2024-10-19 12:06:07 -0400

    nodejs debounce

---
+++ Week-11/11.1/useDebounce-InNodejs/index.js
@@ -0,0 +1,16 @@
+function searchBackend() {
+    console.log("request sent to backend")
+}
+
+
+function debouncedSearchBackend() {
+    // start a clock for 30 ms
+
+}
+
+debouncedSeachBackend()
+debouncedSeachBackend()
+debouncedSeachBackend()
+debouncedSeachBackend()
+debouncedSeachBackend()
+debouncedSeachBackend()
