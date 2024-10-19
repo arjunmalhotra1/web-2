c41329cacaeea8ee52e2725cd0ea91c28ef42bcb
arjunmalhotra1 <am2cj@virginia.edu>
2024-10-19 12:18:35 -0400

    some checkpoint without the slides code by Harkirat

--- Week-11/11.1/useDebounce/src/App.jsx
+++ Week-11/11.1/useDebounce/src/App.jsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
+import { useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function useDebounce(originalfn) {
+  const currentClock = useRef() // Because we don't want the component to change.
+
+  const fn = () => {
+    clearTimeout(currentClock.current)
+    currentClock.current = setTimeout(originalfn, 200)
+  }
+  return fn  
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  function sendDataToBackend() {
+    fetch("api.amazon.com/search")
+  }
+
+  const debouncedFn = useDebounce(sendDataToBackend)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   <input type="text" onChange={debouncedFn}></input>
+   </>
   )
 }
 
