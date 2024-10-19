78c084d64c510109717353281eed583a49d1fce2
arjunmalhotra1 <am2cj@virginia.edu>
2024-10-19 11:04:15 -0400

    fetching

--- Week-11/11.1/custom-hooks/src/App.jsx
+++ Week-11/11.1/custom-hooks/src/App.jsx
@@ -1,21 +1,26 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [post, setPost] = useState({})
-
+  
+  async function getPosts() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts/1")
+    const json = await response.json();
+    setPost(json)
+  }
 
 
 // In react function to useEffect cannot be async hence we take that function out.
-  useEffect(async () => {
+  useEffect(() => {
     getPosts();
   })
 
   return (
     <div>
-       hi there
+       {post.title}
     </div>
   )
 }
