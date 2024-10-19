51bbdf1cfa0033ef6f3cba2d3fbb499f4ac6b64f
arjunmalhotra1 <am2cj@virginia.edu>
2024-10-19 12:09:40 -0400

    checkpoint for debouncing

--- Week-11/11.1/useDebounce-InNodejs/index.js
+++ Week-11/11.1/useDebounce-InNodejs/index.js
@@ -1,10 +1,14 @@
+let currentClock
+
 function searchBackend() {
     console.log("request sent to backend")
 }
 
 
 function debouncedSearchBackend() {
-    // start a clock for 30 ms
+    clearTimeout(currentClock) // stops the old clock and then
+    // we start a clock for 30 ms
+    currentClock = setTimeout(searchBackend, 30)// creates a new clock
 
 }
 
