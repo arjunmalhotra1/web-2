20d2c49c23fdb6a02e358748f2c495693499fb18
arjunmalhotra1 <am2cj@virginia.edu>
2024-10-20 10:50:59 -0400

    checkpoint

--- Week-11/11.2/context-api/src/App.jsx
+++ Week-11/11.2/context-api/src/App.jsx
@@ -7,11 +7,46 @@ import './App.css'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-   <div>Count: {count}</div>
+   <>
+   <Counter/>
+   </>
   )
+}
+
+
+function Counter() {
+  const [count,setCount] = useState(0)
+  return <div>
+    {count}
+    <Increase setCount={setCount}/>
+    <Decrease setCount={setCount}/>
+  </div>
+}
+
+function Decrease({setCount}) {
+
+  function decrease() {
+    setCount(c => c-1)
+  }
+
+  return <div>
+    <button onclick={decrease}>Decrease</button>
+  </div>
+}
+
+
+function Increase(setCount) {
+
+  function increase() {
+    setCount(c => c+1)
+  }
+
+  return <div>
+    <button onclick={increase}>Increase</button>
+  </div>
 }
 
 export default App
