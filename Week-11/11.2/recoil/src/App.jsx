bfcdaa0ae18d8aed13880cf0c41329ef2cc86ab6
arjunmalhotra1 <am2cj@virginia.edu>
2024-10-20 11:02:13 -0400

    wrapping in recoil root & removing comment, because now we will use RecoilRoot

--- Week-11/11.2/recoil/src/App.jsx
+++ Week-11/11.2/recoil/src/App.jsx
@@ -3,13 +3,57 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+
+
+function App() {
   return (
-    <>
-    </>
+   <>
+   <RecoilRoot>
+   <Counter/>
+   </RecoilRoot>
+   </>
   )
+}
+
+
+// Here when count changes the Increase, DEcrease and CurrentCount component all re-render.
+function Counter() {
+  const [count,setCount] = useState(0)
+  return <div>
+    <CurrentCount count={count} />
+    <Increase setCount={setCount}/>
+    <Decrease setCount={setCount}/>
+  </div>
+}
+
+function CurrentCount({count}) {
+    return <div>
+      {count}
+    </div>
+}
+
+function Decrease({setCount}) {
+
+  function decrease() {
+    setCount(c => c-1)
+  }
+
+  return <div>
+    <button onClick={decrease}>Decrease</button>
+  </div>
+}
+
+
+function Increase({setCount}) {
+
+  function increase() {
+    setCount(c => c+1)
+  }
+
+  return <div>
+    <button onClick={increase}>Increase</button>
+  </div>
 }
 
 export default App
