a5ac2709bc71806a5bb86ded4c113b1a41126ae2
arjunmalhotra1 <am2cj@virginia.edu>
2024-10-26 10:04:58 -0400

    error with simple code

--- Week-9/9.2-Offline-Lists-children-lifecycleevents-errorboundaries-fragments/Lists-And-Keys/src/App.jsx
+++ Week-9/9.2-Offline-Lists-children-lifecycleevents-errorboundaries-fragments/Lists-And-Keys/src/App.jsx
@@ -3,27 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// Even with this simple code we see the error.
+// "Each" child in a list should have a unique key property.
+
 function App() {
-  const todos =[{
-    title: "Go To Gym",
-    done: false
-  },{
-    title: "Eat Food",
-    done: true
-  }]
-
-  // Here we concert array of Todos to an array of components. & we render them.
-  const todosComponent = todos.map(todo => <Todo title={todo.title} done={todo.done} />)
-
+  
   return (
     <div>
-      {/* {todosComponent} */}
-      
-      {/* What actuaually happens is this:
       {[
-        <Todo/>,
-        <Todo/>
-      ]} */}
+        <Todo title={"Go to gym"} done={false}/>,
+        <Todo title={"eat food"} done={false}/>
+      ]}
     </div>
   )
 }
