ee8a28e911745f4efd3995d128d0f5ef7ebc2741
arjunmalhotra1 <am2cj@virginia.edu>
2024-11-02 11:32:37 -0400

    fonts

--- Week-13-Tailwind/13.1-Tailwind/1.basic-react/basic-react/src/App.jsx
+++ Week-13-Tailwind/13.1-Tailwind/1.basic-react/basic-react/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
      <div className='grid grid-cols-12'>
-        <div className='col-span-12 sm:col-span-5 bg-red-500'>
+        <div className='col-span-12 sm:col-span-5 bg-red-500 text-8xl'>
           hi
         </div>
         <div className='col-span-12 sm:col-span-5 bg-green-300'>
