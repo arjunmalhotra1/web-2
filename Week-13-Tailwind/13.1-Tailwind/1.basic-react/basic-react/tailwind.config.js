6c4d0e2ac983d7b8ad8a60c6afd84d6deea31c2a
arjunmalhotra1 <am2cj@virginia.edu>
2024-11-02 10:56:44 -0400

    add tailwind paths

--- Week-13-Tailwind/13.1-Tailwind/1.basic-react/basic-react/tailwind.config.js
+++ Week-13-Tailwind/13.1-Tailwind/1.basic-react/basic-react/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ], // content is where are we going to write Tailwind classes. Go to all these places and convert the tailwind code to css code
   theme: {
     extend: {},
   },
