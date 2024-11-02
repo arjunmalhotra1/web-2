85619d2b64e1c7e7ca034a723ae91a861d911551
arjunmalhotra1 <am2cj@virginia.edu>
2024-11-02 12:01:24 -0400

    OTP checkpoint

---
+++ Week-13-Tailwind/13.1-Tailwind/2.First-project/First-project/src/components/otp.jsx
@@ -0,0 +1,17 @@
+export function Otp() {
+
+    
+    return <div>
+        <SubOtpBox/>
+        <SubOtpBox/>
+        <SubOtpBox/>
+        <SubOtpBox/>
+        <SubOtpBox/>
+    </div>
+}
+
+function SubOtpBox() {
+    return <div>
+        <input type="text" className="m-2 w-[40px] h-[50px] rounded-xl bg-blue-500 outline-none px-4 text-white"></input>
+    </div>
+}
