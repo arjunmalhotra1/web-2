1eab16b06676f7a07e5b028cb0e7d3c3e7de4c77
arjunmalhotra1 <am2cj@virginia.edu>
2024-11-02 11:18:36 -0400

    add ing mid size green

--- Week-13-Tailwind/13.1-Tailwind/1.basic-react/basic-react/src/App.jsx
+++ Week-13-Tailwind/13.1-Tailwind/1.basic-react/basic-react/src/App.jsx
@@ -4,12 +4,7 @@ function App() {
 
   return (
     <>
-     {/* It looks like when the sm break point has been hit then render blue else red. But opposite happens
-        When the screen is big then we see blue else we see red Why?
-        Because Tailwind is mobile design.
-        sm:bg-blue-300 is a prefix utility. Unprefix utilities take effect at all screen sizes and prefix one take place/appear at the break point and above.
-       */}
-     <div className='sm:bg-blue-300 bg-red-300'>
+     <div className='md:bg-green-300 sm:bg-blue-300 bg-red-300'>
       hi there
      </div>
     </>
