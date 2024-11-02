3bfdf31848338e3ced2a24d2871fa154a24c5cf0
arjunmalhotra1 <am2cj@virginia.edu>
2024-11-02 11:00:56 -0400

    more clean up

--- Week-13-Tailwind/13.1-Tailwind/1.basic-react/basic-react/src/App.jsx
+++ Week-13-Tailwind/13.1-Tailwind/1.basic-react/basic-react/src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='background-blue-300'>hi</div>
+      <div className='bg-blue-300'>hi</div>
     </>
   )
 }
