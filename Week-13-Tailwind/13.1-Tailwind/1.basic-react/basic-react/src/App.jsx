d19029ae70fd6338f5ad6dd3116f10975ba55a91
arjunmalhotra1 <am2cj@virginia.edu>
2024-11-02 11:27:37 -0400

    format

--- Week-13-Tailwind/13.1-Tailwind/1.basic-react/basic-react/src/App.jsx
+++ Week-13-Tailwind/13.1-Tailwind/1.basic-react/basic-react/src/App.jsx
@@ -5,15 +5,15 @@ function App() {
   return (
     <>
      <div className='grid grid-cols-12'>
-      <div className='col-span-12 sm:col-span-5 bg-red-300'>
+        <div className='col-span-12 sm:col-span-5 bg-red-300'>
+          hi
+        </div>
+        <div className='col-span-12 sm:col-span-5 bg-green-300'>
         hi
-      </div>
-      <div className='col-span-12 sm:col-span-5 bg-green-300'>
-      hi
-      </div>
-      <div className='col-span-12 sm:col-span-2 bg-blue-300'>
-        hi
-      </div>
+        </div>
+        <div className='col-span-12 sm:col-span-2 bg-blue-300'>
+          hi
+        </div>
      </div>
     </>
   )
