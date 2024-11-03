fb9b73a0827933cc4068151dc12e1504b45f6213
arjunmalhotra1 <am2cj@virginia.edu>
2024-11-03 10:14:45 -0500

    Playing with  Sidebar2Transition

--- Week-13-Tailwind/13.2-continue-Tailwind/week-13-otp-box/src/App.jsx
+++ Week-13-Tailwind/13.2-continue-Tailwind/week-13-otp-box/src/App.jsx
@@ -1,12 +1,14 @@
 
 import './App.css'
 import { SidebarClass1 } from './components/answers/1-basic-project'
+import { Sidebar2Transition } from './components/sidebars/Sidebar2Transition'
 
 function App() {
 
   return (
     <div>
       <SidebarClass1 />
+      {/* <Sidebar2Transition /> */}
     </div>
   )
 }
