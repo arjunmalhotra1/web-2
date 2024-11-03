a05e7c9f018e7d1fee4dc86abeac8b343223df33
arjunmalhotra1 <am2cj@virginia.edu>
2024-11-03 10:18:01 -0500

    some chckpoint

--- Week-13-Tailwind/13.2-continue-Tailwind/week-13-otp-box/src/components/answers/1-basic-project.jsx
+++ Week-13-Tailwind/13.2-continue-Tailwind/week-13-otp-box/src/components/answers/1-basic-project.jsx
@@ -1,10 +1,11 @@
 // Adding the 2 comments
 // sm, md, lg, xl, 2xl
 // hidden md:block - This means default is hidden but beyond "md:block" it is visible.
+//  md:w-96 w-8 - This means - width is 8 by default and when we go abobve md then it is 96
 
 export function SidebarClass1() {
     return <div className="flex">
-        <div className="hidden md:block w-96 h-screen">
+        <div className="transition-all duration-1000 md:block md:w-96 w-8 h-screen ">
             Sidebar
         </div>
         <div className="bg-green-800 w-full h-screen">
