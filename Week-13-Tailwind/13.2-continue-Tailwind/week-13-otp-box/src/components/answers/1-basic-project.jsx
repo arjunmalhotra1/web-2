a8db3901c03d9cfa4d8c4e6eb54d4ffbb1da89e9
arjunmalhotra1 <am2cj@virginia.edu>
2024-11-03 10:26:31 -0500

    some checkpoint

--- Week-13-Tailwind/13.2-continue-Tailwind/week-13-otp-box/src/components/answers/1-basic-project.jsx
+++ Week-13-Tailwind/13.2-continue-Tailwind/week-13-otp-box/src/components/answers/1-basic-project.jsx
@@ -3,9 +3,11 @@
 // hidden md:block - This means default is hidden but beyond "md:block" it is visible.
 //  md:w-96 w-8 - This means - width is 8 by default and when we go abobve md then it is 96
 
+// Note: translate-x-96 and -translate-x-96 are different directions.
+
 export function SidebarClass1() {
     return <div className="flex">
-        <div className="transition-all duration-1000 md:block md:w-96 w-8 h-screen ">
+        <div className="transition-all duration-1000 md:block md:w-96 w-0 h-screen ">
             Sidebar
         </div>
         <div className="bg-green-800 w-full h-screen">
