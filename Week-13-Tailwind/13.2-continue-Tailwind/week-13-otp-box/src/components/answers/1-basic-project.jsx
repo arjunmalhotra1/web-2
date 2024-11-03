55359ad9c034d1a9feebd2cd4de8b7d4ceed65db
arjunmalhotra1 <am2cj@virginia.edu>
2024-11-03 10:30:57 -0500

    checkpoint

--- Week-13-Tailwind/13.2-continue-Tailwind/week-13-otp-box/src/components/answers/1-basic-project.jsx
+++ Week-13-Tailwind/13.2-continue-Tailwind/week-13-otp-box/src/components/answers/1-basic-project.jsx
@@ -7,7 +7,7 @@
 
 export function SidebarClass1() {
     return <div className="flex">
-        <div className="transition-all duration-1000 md:block md:w-96 w-0 h-screen ">
+        <div className="transition-all delay-1000 duration-1000 md:block md:w-96 w-0 h-screen ">
             Sidebar
         </div>
         <div className="bg-green-800 w-full h-screen">
