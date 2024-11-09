00275b2cb4f584d12cfb7feb73588a5ca0b6d3bd
arjunmalhotra1 <am2cj@virginia.edu>
2024-11-09 10:07:32 -0500

    Writing a function in typescript

--- Week-14-Typescript/14.1-Typescript/index.ts
+++ Week-14-Typescript/14.1-Typescript/index.ts
@@ -1,3 +1,10 @@
-let x:number = 1;
+function greet(firstName:string) {
+    console.log("Hello "+firstName)
+}
 
-console.log(x)
+greet("firstName")
+
+// let x:any =1;
+// x=true
+// x="firstName"
+// x="random"
