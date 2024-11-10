8848a00d5bd42bade8c5fdc88ea08686c2ab094a
arjunmalhotra1 <am2cj@virginia.edu>
2024-11-10 09:49:54 -0500

    need for interface

---
+++ Week-14-Typescript/14.2-typescript-continued/src/index.ts
@@ -0,0 +1,16 @@
+// Interfaces and types
+
+// We cannot use "user" as a primitive type so we use interfaces to define a type.
+let user = {
+    name: "harkirat",
+    age :21,
+    address: {
+        city: "Chandigarh",
+        country: "India",
+        pincode: 1506994
+    }
+}
+
+function isLegalToVote(): boolean {
+
+}
