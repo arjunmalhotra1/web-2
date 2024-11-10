fdb3c86b2389974a102fabf23c0e4cdb515f286e
arjunmalhotra1 <am2cj@virginia.edu>
2024-11-10 10:30:25 -0500

    checkpoint

--- Week-14-Typescript/14.2-typescript-continued/src/index.ts
+++ Week-14-Typescript/14.2-typescript-continued/src/index.ts
@@ -1,6 +1,7 @@
 interface people {
     name: string,
     age:number,
+    isLegal():boolean
 }
 
 
@@ -13,7 +14,22 @@ class Manager implements people{
         this.name = name;
         this.age = age;
     }
+
+    isLegal() {
+        return this.age > 18
+    }
+}
+
+// This could be done but these objects cannot extend other classes.
+let user2 = {
+    name:"har",
+    age: 21,
+    isLegal():boolean {
+        return user.age>18
+    }
 }
 
 let user = new Manager("John",30)
 console.log(user)
+
+console.log(user.isLegal())
