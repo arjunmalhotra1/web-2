3a5eab59e4096948c0c1c8b454096a1f1bcc96ea
arjunmalhotra1 <am2cj@virginia.edu>
2024-11-10 10:20:29 -0500

    class and object

--- Week-14-Typescript/14.2-typescript-continued/src/index.ts
+++ Week-14-Typescript/14.2-typescript-continued/src/index.ts
@@ -1,17 +1,19 @@
 interface people {
     name: string,
     age:number,
-    // Greet is a function that doesn't take anything as input and returns a "string"
-    greet: () => string,
 }
 
-let person: people = {
-    name:"har",
-    age:21,
-    greet:() => {
-        return "hi" + this.name;
+
+// Class give us additional thing like inheritance etc.
+class Manager implements people{
+    name:string;
+    age: number;
+
+    constructor(name: string, age: number){
+        this.name = name;
+        this.age = age;
     }
 }
 
-let greeting = person.greet()
-console.log("greeting: ",greeting)
+let user = new Manager("John",30)
+console.log(user)
