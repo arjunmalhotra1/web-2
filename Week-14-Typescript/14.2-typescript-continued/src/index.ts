c8e5d5f9ebffbf38f3c009df4624401b77f119a3
arjunmalhotra1 <am2cj@virginia.edu>
2024-11-10 10:07:30 -0500

    something

--- Week-14-Typescript/14.2-typescript-continued/src/index.ts
+++ Week-14-Typescript/14.2-typescript-continued/src/index.ts
@@ -1,45 +1,8 @@
-// Interfaces and types
-
-interface Address {
-    city: string;
-    country: string;
-    pincode: number;
-
+interface people {
+    name: string,
+    age:number,
+    // Greet is a function that doesn't take anything as input and returns a "string"
+    // Both below are same.
+    greet: () => string,
+    greet2(): () => string,
 }
-
-interface User {
-    name: string;
-    age: number;
-    address: Address
-   
-}
-
-// We do not want "office address to be repeated"
-interface Office {
-    address: Address
-}
-
-
-
-let user:User = {
-    name: "harkirat",
-    age :21,
-    address: {
-        city: "Chandigarh",
-        country: "India",
-        pincode: 1506994
-    }
-}
-
-
-// let user2:User = {
-//     name: "rama",
-//     age :22,
-// }
-
-
-function isLegalToVote(user: User): boolean {
-    return user.age >= 18;
-}
-
-console.log(isLegalToVote(user))
