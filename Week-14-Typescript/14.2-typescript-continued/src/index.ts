ff8e775a82f59c067b1b8654688b6f626f18dcf5
arjunmalhotra1 <am2cj@virginia.edu>
2024-11-10 10:43:38 -0500

    Interface and types

--- Week-14-Typescript/14.2-typescript-continued/src/index.ts
+++ Week-14-Typescript/14.2-typescript-continued/src/index.ts
@@ -1,35 +1,12 @@
-interface people {
-    name: string,
-    age:number,
-    isLegal():boolean
-}
+// Types
 
-
-// Class give us additional thing like inheritance etc.
-class Manager implements people{
-    name:string;
+interface User {
+    name: string;
     age: number;
-
-    constructor(name: string, age: number){
-        this.name = name;
-        this.age = age;
-    }
-
-    isLegal() {
-        return this.age > 18
-    }
 }
 
-// This could be done but these objects cannot extend other classes.
-let user2 = {
-    name:"har",
-    age: 21,
-    isLegal():boolean {
-        return user.age>18
-    }
+
+type User = {
+    name:string;
+    age: number
 }
-
-let user = new Manager("John",30)
-console.log(user)
-
-console.log(user.isLegal())
