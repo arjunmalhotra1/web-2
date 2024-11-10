0e36aa168212527a7da3f40cb6dced8827a44735
arjunmalhotra1 <am2cj@virginia.edu>
2024-11-10 10:10:03 -0500

    greet function in the interface.

--- Week-14-Typescript/14.2-typescript-continued/src/index.ts
+++ Week-14-Typescript/14.2-typescript-continued/src/index.ts
@@ -2,7 +2,16 @@ interface people {
     name: string,
     age:number,
     // Greet is a function that doesn't take anything as input and returns a "string"
-    // Both below are same.
     greet: () => string,
-    greet2():string,
 }
+
+let person: people = {
+    name:"har",
+    age:21,
+    greet:() => {
+        return "hi har"
+    }
+}
+
+let greeting = person.greet()
+console.log("greeting: ",greeting)
