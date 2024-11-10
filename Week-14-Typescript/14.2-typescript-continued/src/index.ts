d66a3b14173bac740eb0aff59ae4965a26880ebb
arjunmalhotra1 <am2cj@virginia.edu>
2024-11-10 09:52:15 -0500

    error

--- Week-14-Typescript/14.2-typescript-continued/src/index.ts
+++ Week-14-Typescript/14.2-typescript-continued/src/index.ts
@@ -1,16 +1,27 @@
 // Interfaces and types
 
-// We cannot use "user" as a primitive type so we use interfaces to define a type.
-let user = {
+
+interface User {
+    name: string;
+    age: number;
+    address: {
+        city: string;
+        country: string;
+        pincode: number;
+    };
+}
+
+
+// When we define a custom type & we don't give pincode, then we get a compile time error.
+let user:User = {
     name: "harkirat",
     age :21,
     address: {
         city: "Chandigarh",
-        country: "India",
-        pincode: 1506994
+        country: "India"
     }
 }
 
-function isLegalToVote(): boolean {
+// function isLegalToVote(): boolean {
 
-}
+// }
