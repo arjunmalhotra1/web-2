f0e6ea677f82601de2536449ccd391db5dc5f6de
arjunmalhotra1 <am2cj@virginia.edu>
2024-11-10 10:45:38 -0500

    some checkpoint

--- Week-14-Typescript/14.2-typescript-continued/src/index.ts
+++ Week-14-Typescript/14.2-typescript-continued/src/index.ts
@@ -1,12 +1,20 @@
 // Types
 
-interface User {
-    name: string;
-    age: number;
+interface User2 {
+    name: string
+    age: number
 }
-
 
 type User = {
     name:string;
     age: number
 }
+
+
+function isLegal(user:User) {
+    return user.age > 18 
+}
+
+// We can't implement a type
+// Types let us do "Unions" & "Intersections"
+
