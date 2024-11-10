3f8e37cb6de7d8e5b88b56cb961c0dedf976652a
arjunmalhotra1 <am2cj@virginia.edu>
2024-11-10 10:59:08 -0500

    Intersection and Union - Incomplete

--- Week-14-Typescript/14.2-typescript-continued/src/index.ts
+++ Week-14-Typescript/14.2-typescript-continued/src/index.ts
@@ -1,20 +1,48 @@
-// Types
 
-interface User2 {
-    name: string
-    age: number
-}
-
-type User = {
+// Intersection 
+type Employee = {
     name:string;
-    age: number
+    startDate: string
+}
+
+type Manager = {
+    name: string;
+    department: string
+}
+
+type TeamLead = Employee & Manager
+
+let e: Employee = {
+    name:"harkirat",
+    startDate: "01-02-2004"
+}
+
+// Union
+let m: Manager = {
+    name:"harkirat",
+    department: "electricity"
+}
+
+let t: TeamLead = {
+   name:"harkirat", 
+   startDate:"01-02-2020",
+   department: "electicity"
 }
 
 
-function isLegal(user:User) {
-    return user.age > 18 
+type GoodUser = {
+    name: string;
+    gift: string
 }
 
-// We can't implement a type
-// Types let us do "Unions" & "Intersections"
+type BadUser = {
+    name: string;
+    ip: string
+}
 
+type User = GoodUser | BadUser;
+
+const user: User = {
+    name:"harkirat",
+    ip:"123.112.112"
+}
