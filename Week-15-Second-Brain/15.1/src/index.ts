63fb46dbebc39054e2f322fa21398417ca58ca02
arjunmalhotra1 <am2cj@virginia.edu>
2024-11-16 10:19:07 -0500

    Add some code

--- Week-15-Second-Brain/15.1/src/index.ts
+++ Week-15-Second-Brain/15.1/src/index.ts
@@ -4,19 +4,32 @@ import mongoose from "mongoose"
 
 import jwt from "jsonwebtoken"
 
+const app = express();
 
-const app = express()
+app.post("/api/v1/signup", (req, res) => {
 
-app.post("/api/v1/signin",(req,res)=>{})
+})
 
-app.post("/api/v1/signin",(req,res)=>{})
+app.post("/api/v1/signin", (req, res) => {
 
-app.post("/api/v1/signin",(req,res)=>{})
+})
 
-app.post("/api/v1/signin",(req,res)=>{})
+app.post("/api/v1/content", (req, res) => {
 
-app.post("/api/v1/signin",(req,res)=>{})
+})
 
-app.get("/api/v1/signin",(req,res)=>{})
+app.get("/api/v1/content", (req, res) => {
 
-app.delete("/api/v1/signin",(req,res)=>{})
+})
+
+app.delete("/api/v1/content", (req, res) => {
+
+})
+
+app.post("/api/v1/brain/share", (req, res) => {
+
+})
+
+app.get("/api/v1/brain/:shareLink", (req, res) => {
+
+})
