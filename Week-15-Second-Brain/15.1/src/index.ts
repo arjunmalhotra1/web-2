6807a9c39d27f7ba71db47ca1c950f0149e2356f
arjunmalhotra1 <am2cj@virginia.edu>
2024-11-17 09:39:20 -0500

    checkpoint from 15.1

--- Week-15-Second-Brain/15.1/src/index.ts
+++ Week-15-Second-Brain/15.1/src/index.ts
@@ -1,48 +1,98 @@
-import express from "express"
 
-import mongoose from "mongoose"
-
-import jwt from "jsonwebtoken"
-import { UserModel } from "./db";
+import express from "express";
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import { ContentModel, UserModel } from "./db";
+import { JWT_PASSWORD } from "./config";
+import { userMiddleware } from "./middleware";
 
 const app = express();
+app.use(express.json());
 
 app.post("/api/v1/signup", async (req, res) => {
-    // zod validation & Hashpassword
-    const username = req.body.username
-    const password = req.body.password
+    // TODO: zod validation , hash the password
+    const username = req.body.username;
+    const password = req.body.password;
 
-    try {}
-    await UserModel.create({
-        username:username,
-        password:password
+    try {
+        await UserModel.create({
+            username: username,
+            password: password
+        }) 
+
+        res.json({
+            message: "User signed up"
+        })
+    } catch(e) {
+        res.status(411).json({
+            message: "User already exists"
+        })
+    }
+})
+
+app.post("/api/v1/signin", async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const existingUser = await UserModel.findOne({
+        username,
+        password
+    })
+    if (existingUser) {
+        const token = jwt.sign({
+            id: existingUser._id
+        }, JWT_PASSWORD)
+
+        res.json({
+            token
+        })
+    } else {
+        res.status(403).json({
+            message: "Incorrrect credentials"
+        })
+    }
+})
+
+app.post("/api/v1/content", userMiddleware, async (req, res) => {
+    const link = req.body.link;
+    const type = req.body.type;
+    await ContentModel.create({
+        link,
+        type,
+        //@ts-ignore
+        userId: req.userId,
+        tags: []
     })
 
     res.json({
-        message: "User signed up"
+        message: "Content added"
     })
-} catch(e) {
-    res.status(411).json() {
-        message: "User already exists"
-    }
-}
-
+    
 })
 
-app.post("/api/v1/signin", (req, res) => {
-
+app.get("/api/v1/content", userMiddleware, async (req, res) => {
+    // @ts-ignore
+    const userId = req.userId;
+    const content = await ContentModel.find({
+        userId: userId
+    }).populate("userId", "username")
+    res.json({
+        content
+    })
 })
 
-app.post("/api/v1/content", (req, res) => {
+app.delete("/api/v1/content", userMiddleware, async (req, res) => {
+    const contentId = req.body.contentId;
 
-})
+    await ContentModel.deleteMany({
+        contentId,
+        // @ts-ignore
+        userId: req.userId
+    })
 
-app.get("/api/v1/content", (req, res) => {
-
-})
-
-app.delete("/api/v1/content", (req, res) => {
-
+    res.json({
+        message: "Deleted"
+    })
 })
 
 app.post("/api/v1/brain/share", (req, res) => {
@@ -53,4 +103,4 @@ app.get("/api/v1/brain/:shareLink", (req, res) => {
 
 })
 
-app.listen(8765)
+app.listen(3000);
