a8e9060d8a4dd6e143e71714b93d2862b7b2889e
arjunmalhotra1 <am2cj@virginia.edu>
2024-11-17 10:31:15 -0500

    some checkpoint

--- Week-15-Second-Brain/15.2-frontend/brainly-frontend/src/components/ui/Button.tsx
+++ Week-15-Second-Brain/15.2-frontend/brainly-frontend/src/components/ui/Button.tsx
@@ -15,11 +15,16 @@ const variantStyles = {
 }
 
 const defaultStyles = "rounded-md pd-4"
+const sizeStyles = {
+    "sm": "py-1 px-2",
+    "md": "py-2 px-4",
+    "lg": "py-2 px-6"
+}
 
 
 export const Button = (props: ButtonProps) => {
   
-    return <button className={variantStyles[props.variant]}></button>
+    return <button className={`${variantStyles[props.variant]} ${defaultStyles} ${sizeStyles[props.size]}`}></button>
 }
 
 <Button variant="primary" size="md" onClick={() =>{}} text={"asd"}/>
