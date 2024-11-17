e35595a0101f38adf1de55d38f87dbdee2efac63
arjunmalhotra1 <am2cj@virginia.edu>
2024-11-17 09:56:47 -0500

    adding a comment.

--- Week-15-Second-Brain/15.2-frontend/brainly-frontend/tailwind.config.js
+++ Week-15-Second-Brain/15.2-frontend/brainly-frontend/tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}", // Any file in the src folder can have tailwind.
   ],
   theme: {
     extend: {},
