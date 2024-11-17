dd87f906930d730d064af9f5ff5d864b138a1d59
arjunmalhotra1 <am2cj@virginia.edu>
2024-11-17 10:02:28 -0500

    some checkpoint

--- Week-15-Second-Brain/15.2-frontend/brainly-frontend/src/components/ui/Button.tsx
+++ Week-15-Second-Brain/15.2-frontend/brainly-frontend/src/components/ui/Button.tsx
@@ -1,6 +1,10 @@
 export interface buttonProps {
     variant:"primary" | "secondary",
     size: "sm" | "md" | "lg"
+    text: string,
+    startIcon:any;
+    endIcon:any;
+    onClick: () => void
 }
 
 
@@ -9,4 +13,4 @@ export const Button = () => {
     return <button></button>
 }
 
-{/* <Button variant="primary" size="md" onClick={} text{}/> */}
+<Button variant="primary" size="md" onClick={} text{}/>
