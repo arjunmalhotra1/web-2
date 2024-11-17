65712b6942e4425bcea02e545c7a038b5069a8f4
arjunmalhotra1 <am2cj@virginia.edu>
2024-11-17 10:06:19 -0500

    Make startIcon and endIcon optional

--- Week-15-Second-Brain/15.2-frontend/brainly-frontend/src/components/ui/Button.tsx
+++ Week-15-Second-Brain/15.2-frontend/brainly-frontend/src/components/ui/Button.tsx
@@ -2,8 +2,8 @@ export interface ButtonProps {
     variant:"primary" | "secondary",
     size: "sm" | "md" | "lg"
     text: string,
-    startIcon:any;
-    endIcon:any;
+    startIcon?:any;
+    endIcon?:any;
     onClick: () => void
 }
 
@@ -14,4 +14,4 @@ export const Button = (props: ButtonProps) => {
     return <button></button>
 }
 
-<Button startIcon={"+"} endIcon={"+"}variant="primary" size="md" onClick={() =>{}} text={"asd"}/>
+<Button variant="primary" size="md" onClick={() =>{}} text={"asd"}/>
