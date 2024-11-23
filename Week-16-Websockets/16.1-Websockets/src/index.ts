fbc026114734389a058512297a74aad9e7d7da36
arjunmalhotra1 <am2cj@virginia.edu>
2024-11-23 10:44:45 -0500

    ping pong

--- Week-16-Websockets/16.1-Websockets/src/index.ts
+++ Week-16-Websockets/16.1-Websockets/src/index.ts
@@ -12,12 +12,14 @@ const wss = new WebSocketServer({port:8789})
 // This is an Event handler
 wss.on("connection",function(socket){
     console.log("user connected")
-    setInterval(()=>{
-        socket.send("Current price of Solana is"+Math.random())
-    },500)
+    // setInterval(()=>{
+    //     socket.send("Current price of Solana is"+Math.random())
+    // },500)
 
     socket.on("message",(e)=>{
-        console.log(e.toString())
+        if(e.toString()==="ping") {
+            socket.send("pong")
+        }
     })
     
 })
