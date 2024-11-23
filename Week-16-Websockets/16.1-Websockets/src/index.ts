4798fda4e21e5c507aaa2987b1889fcc38e4dc08
arjunmalhotra1 <am2cj@virginia.edu>
2024-11-23 10:23:36 -0500

    some checkpoint

--- Week-16-Websockets/16.1-Websockets/src/index.ts
+++ Week-16-Websockets/16.1-Websockets/src/index.ts
@@ -1,1 +1,18 @@
-console.log("hi")
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({port:8789})
+
+
+
+// app.get("/users",(req,res)=>{
+    
+// })
+// On a new connection call the function. Give me the "socket" of the person. This is like
+// "req, res"
+// This is an Event handler
+wss.on("connection",function(socket){
+    console.log("user connected")
+    socket.send("hello")
+})
+
+
