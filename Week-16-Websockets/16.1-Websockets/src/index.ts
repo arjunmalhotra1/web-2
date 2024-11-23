2f97155c3a10f7801f9afc413b3825a201ab2cb5
arjunmalhotra1 <am2cj@virginia.edu>
2024-11-23 10:28:15 -0500

    some checkpoint

--- Week-16-Websockets/16.1-Websockets/src/index.ts
+++ Week-16-Websockets/16.1-Websockets/src/index.ts
@@ -12,7 +12,14 @@ const wss = new WebSocketServer({port:8789})
 // This is an Event handler
 wss.on("connection",function(socket){
     console.log("user connected")
-    socket.send("hello")
+    setInterval(()=>{
+        socket.send("Current price of Solana is"+Math.random())
+    },500)
+
+    socket.on("message",(e)=>{
+        console.log(e)
+    })
+    
 })
 
 
