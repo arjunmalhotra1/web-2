f4690af4a3168c5e9a61eab5a9746fecf8761213
arjunmalhotra1 <am2cj@virginia.edu>
2024-11-23 10:47:58 -0500

    some checkpoint

--- Week-16-Websockets/16.1-Websockets/ping-pong-ui/src/App.tsx
+++ Week-16-Websockets/16.1-Websockets/ping-pong-ui/src/App.tsx
@@ -5,9 +5,14 @@ import './App.css'
 
 function App() {
 
+  function sendMessage() {
+    
+  }
+
   return (
     <div>
-      hi there
+      <input type='text' placeholder='Message...'></input>
+      <button onClick={sendMessage}>Send</button>
     </div>
   )
 }
