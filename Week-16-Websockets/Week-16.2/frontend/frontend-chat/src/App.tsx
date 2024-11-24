296edb5163b702435a5b718dcd5c9d1628640c67
arjunmalhotra1 <am2cj@virginia.edu>
2024-11-24 11:09:05 -0500

    some cp

--- Week-16-Websockets/Week-16.2/frontend/frontend-chat/src/App.tsx
+++ Week-16-Websockets/Week-16.2/frontend/frontend-chat/src/App.tsx
@@ -7,8 +7,11 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='h-screen bg-black flex flex-col'>
-      
+    <div className='h-screen bg-black'>
+      <div className='h-[95vh] bg-red-300' ></div>
+      <div className=''>
+        <input type='text w-full p-4'></input>
+      </div>
     </div>
   )
 }
