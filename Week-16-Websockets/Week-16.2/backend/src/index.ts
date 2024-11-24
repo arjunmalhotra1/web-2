539a924270eff38cffa4ce753d0cd1342324022b
arjunmalhotra1 <am2cj@virginia.edu>
2024-11-24 09:55:16 -0500

    some checkpoint

--- Week-16-Websockets/Week-16.2/backend/src/index.ts
+++ Week-16-Websockets/Week-16.2/backend/src/index.ts
@@ -2,7 +2,9 @@ import { WebSocketServer } from "ws";
 
 const wss = new WebSocketServer({port: 8798})
 
+let userCount = 0
 
 wss.on("connection",(socket)=>{
-    
+    userCount = userCount+1
+    console.log("user connected #"+userCount)
 })
