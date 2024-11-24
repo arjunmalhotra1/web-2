66a907d4e4651a63f6a26df0e74413df330d5297
arjunmalhotra1 <am2cj@virginia.edu>
2024-11-24 10:04:32 -0500

    checkpoint

--- Week-16-Websockets/Week-16.2/backend/src/index.ts
+++ Week-16-Websockets/Week-16.2/backend/src/index.ts
@@ -10,7 +10,9 @@ wss.on("connection",(socket)=>{
 
     socket.on("message",(message)=>{
         console.log("message received "+message.toString())
-        //setTimeout(())
-        socket.send(message.toString() + ": sent from server")
+        setTimeout(()=>{
+            socket.send(message.toString() + ": sent from server")    
+        },1000)
+        
     })
 })
