290cd78d65278f1294a59d9785f5ce861635d6ca
arjunmalhotra1 <am2cj@virginia.edu>
2024-11-24 10:34:36 -0500

    some designing

--- Week-16-Websockets/Week-16.2/backend/src/index.ts
+++ Week-16-Websockets/Week-16.2/backend/src/index.ts
@@ -2,8 +2,24 @@ import { WebSocketServer,WebSocket } from "ws";
 
 const wss = new WebSocketServer({port: 8798})
 
-let userCount = 0
-let allSockets:WebSocket[] = []
+// One way
+// let allSockets ={
+//     "room1":[socket1,socket2]
+//     "12334":[socket],
+// }
+
+interface User {
+    socket: WebSocket,
+    room: string
+}
+
+let allSockets: User[] = []
+
+[
+    {socket:socket, room: "room1"},
+    {socket:socket2, room: "room2"},
+    {socket:socket3, room: "room1"},
+]
 
 wss.on("connection",(socket)=>{
     allSockets.push(socket)
