ff6277db1588c3b4f3fd929f354f10ac81d97d78
arjunmalhotra1 <am2cj@virginia.edu>
2024-12-07 10:37:29 -0500

    add user

--- Week-18-Prisma-ORMs/Week-18.1/src/index.ts
+++ Week-18-Prisma-ORMs/Week-18.1/src/index.ts
@@ -1,1 +1,19 @@
-console.log("hello world")
+
+import { PrismaClient } from "@prisma/client";
+
+const client = new PrismaClient()
+
+
+async function createUser() {
+    await client.user.create({
+        data: {
+            username:"username",
+            password: "password",
+            age: 42,
+            city: "city"
+        }
+    })    
+}
+
+createUser()
+
