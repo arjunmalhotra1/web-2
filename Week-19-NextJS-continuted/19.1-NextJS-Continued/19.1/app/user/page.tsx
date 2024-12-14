bfa5bfcba582554990e773fca040f127d08d971d
arjunmalhotra1 <am2cj@virginia.edu>
2024-12-14 10:43:27 -0500

    checkpoint

--- Week-19-NextJS-continuted/19.1-NextJS-Continued/19.1/app/user/page.tsx
+++ Week-19-NextJS-continuted/19.1-NextJS-Continued/19.1/app/user/page.tsx
@@ -6,7 +6,7 @@ export default async function User() {
     const response = await axios.get("http://localhost:3000/api/v1/user/details")
     const data = response.data
 
-    await new Promise(r => setTimeout(r, 5000))
+    await new Promise(r => setTimeout(r, 2000))
  
     console.log("hi")
     return <div>
