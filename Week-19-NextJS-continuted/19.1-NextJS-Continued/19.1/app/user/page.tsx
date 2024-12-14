49873e5938fc2371053819001830d2fe40d14cdd
arjunmalhotra1 <am2cj@virginia.edu>
2024-12-14 10:15:47 -0500

    better way to do in NextJS

--- Week-19-NextJS-continuted/19.1-NextJS-Continued/19.1/app/user/page.tsx
+++ Week-19-NextJS-continuted/19.1-NextJS-Continued/19.1/app/user/page.tsx
@@ -2,24 +2,10 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-export default function User() {
-    const [loading, setLoading] = useState(false);
-    const [data, setData] = useState();
-
-    // Here the rendering is on the client.
-    useEffect(()=>{
-        axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details").then(response => {
-            setData(response.data);
-            setLoading(false)
-        })
-    },[])
-
-if (loading) {
-    return <div>
-        Loading....
-    </div>
-}
-
+export default async function User() {
+    
+    const response = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
+    const data = response.data
 
     return <div>
         User Page
