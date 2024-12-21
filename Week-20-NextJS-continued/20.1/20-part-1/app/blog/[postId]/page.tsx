bf5af55f59fb5e9d587f48a6935afbf37ff11ec8
arjunmalhotra1 <am2cj@virginia.edu>
2024-12-21 10:13:34 -0500

    Dynamic routes

--- Week-20-NextJS-continued/20.1/20-part-1/app/blog/[postId]/page.tsx
+++ Week-20-NextJS-continued/20.1/20-part-1/app/blog/[postId]/page.tsx
@@ -1,6 +1,13 @@
-export default function({params}: any) {
-    const postId = params.postId
+import axios from "axios"
+
+export default async function({params}: any) {
+    const postId = (await params).postId
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+    const data = response.data
     return <div>
         Blog Page {postId}
+        <br></br>
+        title - {data.title}
+        body - {data.body}
     </div>
 }
