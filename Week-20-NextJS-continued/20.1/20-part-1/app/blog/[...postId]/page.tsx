fb9b1f66a4209ffce4da506c53a3dca73c68ec9e
arjunmalhotra1 <am2cj@virginia.edu>
2024-12-21 10:29:34 -0500

    checkpoint

--- Week-20-NextJS-continued/20.1/20-part-1/app/blog/[...postId]/page.tsx
+++ Week-20-NextJS-continued/20.1/20-part-1/app/blog/[...postId]/page.tsx
@@ -1,16 +1,7 @@
 import axios from "axios"
 
 export default async function({params}: any) {
-    const postId = (await params).postId
-    // const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${postId}`)
-    // const data = response.data
-    // return <div>
-    //     Blog Page {postId}
-    //     <br></br>
-    //     title - {data.title}
-    //     body - {data.body}
-    // [...] – Catch-All Segment
     return <div>
-        Blog Page {JSON.stringify(postId)}
+        Blog Page
     </div>
 }
