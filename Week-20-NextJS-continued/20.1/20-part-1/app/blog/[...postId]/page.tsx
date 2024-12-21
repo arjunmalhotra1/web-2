033ca02a8f4feaea2784609d4b17687cd02f85eb
arjunmalhotra1 <am2cj@virginia.edu>
2024-12-21 10:20:17 -0500

    checkpoint [...postId]

---
+++ Week-20-NextJS-continued/20.1/20-part-1/app/blog/[...postId]/page.tsx
@@ -0,0 +1,15 @@
+import axios from "axios"
+
+export default async function({params}: any) {
+    const postId = (await params).postId
+    // const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+    // const data = response.data
+    // return <div>
+    //     Blog Page {postId}
+    //     <br></br>
+    //     title - {data.title}
+    //     body - {data.body}
+    return <div>
+        Blog Page {JSON.stringify(postId)}
+    </div>
+}
