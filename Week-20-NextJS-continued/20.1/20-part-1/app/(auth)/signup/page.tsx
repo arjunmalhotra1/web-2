60479dbb53ea6c336e2111fd4f193626dfb06cf2
arjunmalhotra1 <am2cj@virginia.edu>
2024-12-21 10:00:55 -0500

    some checkpoint

---
+++ Week-20-NextJS-continued/20.1/20-part-1/app/(auth)/signup/page.tsx
@@ -0,0 +1,8 @@
+export default function() {
+    return <div>
+        Signup
+        <input type="text"></input>
+        <input type="text"></input>
+        <button>Sign Up</button>
+    </div>
+}
