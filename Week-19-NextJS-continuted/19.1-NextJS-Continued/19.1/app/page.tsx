c808d2b337695e400fc0a585799c226d1b13394a
arjunmalhotra1 <am2cj@virginia.edu>
2024-12-21 09:32:45 -0500

    checkpoint

--- Week-19-NextJS-continuted/19.1-NextJS-Continued/19.1/app/page.tsx
+++ Week-19-NextJS-continuted/19.1-NextJS-Continued/19.1/app/page.tsx
@@ -1,9 +1,18 @@
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter()
   return (
-  <div>
-    hi there
+  <div className="text-lg w-screen h-screen flex items-center justify-center">
+    <div>
+      Todo Application
+      <button onClick={()=>{
+        router.push("/signin")
+      }}>Sign in</button>
+    </div>
+    <br/>
+
   </div>
   );
 }
